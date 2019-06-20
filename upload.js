a101d9fd07c1f8db2b6fc2bf1fd65e3609d0ce83
ygyash <ygyashgoyal@gmail.com>
2019-06-20 17:59:46 +0530

    Removes the necessity of othetwo servers for the download.

--- upload.js
+++ upload.js
@@ -1,104 +1,68 @@
 var express = require('express');
 var app = express();
 var ip = require('ip');
-var apiRouter = express.Router();
 var fs = require('fs');
 var formidable = require('formidable');
-
+const PORT = process.env.PORT||8086;
 
 
  
 // html file containing upload form
 var upload_html = fs.readFileSync("./public/index.html");
-var qrcode_html = fs.readFileSync("./public/qrcode.html");
 
 app.use(express.static(__dirname + '/public'));
 
- 
-// replace this with the location to save uploaded files
-var upload_path = "./build_stock";
 
 app.get('/',function(req,res){
   
     res.send(upload_html);
 });
 
-app.post('/',function(req,res){
-  
-    res.end(upload_html);
+
+app.get('/upload',function(req,res){
+    res.redirect('/');
 });
-
-
-
+    
 try{
-	app.post('/upload', function (req, res) {
-
+    app.post('/upload', function (req, res) {
+    //console.log("posted");
      var form = new formidable.IncomingForm();
+
         form.parse(req);
         form.maxFileSize = 1024*1024*1024;
 
-    form.on('fileBegin', function (name, file){
-    	//replace this with the ipv4 address of your computer in the network
-    	//this is the link which will be downloaded
-        var updatedPath = "http://"+ip.address()+":8000/build_stock/";
-        var name = file.name;
-        //updatedPath is changed to edit all the spaces and special characters present in the file name
-         for(i=0;i<file.name.length;i++)
-        {  
-            if(file.name[i]==" ")
-            {   
-                updatedPath+="%20";
-                
-            }
-            else if(file.name[i]=="-"){
-                updatedPath += "%2D";
-            }
-            else if(file.name[i]=="_"){
-                updatedPath += "%5F";
-            }
-            else if(file.name[i]=="~"){
-                updatedPath+="%7E";
-            }
-            else
-            {
-                updatedPath+=file.name[i];
-            }
-            //console.log("Char encoded:"+updatedPath);
+    form.on('fileBegin', function (name, file,err){
+
+        if(err){
+            console.log(err);
+            res.redirect('/');
         }
-        console.log(updatedPath);
-        var constant = {
-            "file":
-            {
-                "name":name,
-                "link":updatedPath
-            }
-        };
-        /*writing the file name and the link of the file in a json file
-            which can be accessed by the front end API for creating the QR code 
-        */
-        console.log(file.name);
-       if(file.name!=undefined){
-       	 fs.writeFile(__dirname + "/public"+"/data.json",JSON.stringify(constant,null,2),function(err){
-            if(err) return console.log(err);
-        });
-        file.path = __dirname + '/build_stock/' + name;    
-    }
-    else{
-    	res.sendStatusCode(404);
-    }
-
-
+        if(file.name===""){
+            res.sendStatus(404);
+        }
+        else{
+            //the path of the file from where the file can be downloaded
+            var downloadPath = "http://"+ip.address()+":"+PORT+"/build_stock/"+encodeURIComponent(file.name);
+            var constant = {
+                "name":file.name,
+                "link":downloadPath
+            };
+            
+            //copying the file to the server
+            file.path = __dirname + '/public/build_stock/' + file.name;   
+            //console.log(constant);
+            res.status(200).json({
+                success:true,
+                data:constant
+            });
+        }
     });
-            res.end(qrcode_html);
-
-    
 });
 
 }
 catch(e){
 	console.log(e);
 }
-
- app.listen(8086,function(){
- 	console.log("Sharing your file using QR Code: Running on 8086");
+ app.listen(PORT,function(){
+ 	console.log("QRit: Running on",PORT);
  });
